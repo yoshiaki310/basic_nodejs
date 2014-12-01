b61188ac91d2fe25eeb1c4509d25c0a8fff777f2
Yoshiaki Satoh <yoshiaki.satoh@gmail.com>
2014-12-02 00:03:05 +0900

    lesson5追加。

---
+++ lesson5.ts/server.ts
@@ -0,0 +1,21 @@
+﻿import http = require('http');
+var server : any = http.createServer();
+var httpMessage : string = 'Hello, World';
+server.on('request',function(req,res){
+	res.writeHead(200, {'Content-Type':'Text/Plain'});
+	res.write(httpMessage);
+	res.end();
+});
+server.listen(1337,'localhost');
+console.log('server listening...');
+
+
+
+/*
+import http = require('http');
+var port = process.env.port || 1337
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
+*/
