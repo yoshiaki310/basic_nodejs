2141af92e667c75136fce9323d803b4b360f4722
Yoshiaki Satoh <yoshiaki.satoh@gmail.com>
2014-12-07 23:46:00 +0900

    #13まで

--- lesson5.ts/server.ts
+++ lesson5.ts/server.ts
@@ -1,25 +1,41 @@
 ﻿var http: any = require('http'),
     fs: any = require('fs'),
-    ejs: any = require('ejs');
+    ejs: any = require('ejs'),
+    qs: any = require('querystring');
 
 var server: any = http.createServer();
 
 var httpMessage: string = 'Hello, World';
 var settings = require('./settings.ts');
-var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
+var template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
+var posts = [];
 
-var n: number = 0;
-
-server.on('request', function (req, res) {
-    n++;
+function renderForm(posts, res) {
     var data = ejs.render(template, {
-        title: 'helo',
-        content: '<strong>world !</strong>',
-        n: n,
+        posts: posts,
     });
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(data);
     res.end();
+}
+
+server.on('request', function (req, res) {
+
+    if (req.method === 'POST') {
+        req.data = '';
+        req.on("readable", function () {
+            req.data += req.read();
+        });
+        req.on("end", function () {
+            var query = qs.parse(req.data);
+            posts.push(query.name);
+            renderForm(posts, res);
+        });
+
+
+    } else {
+        renderForm(posts, res);
+    }
 
 });
 server.listen(settings.port, settings.server);
